0cbcc1e04baf049d8b00e8a4077254ce017e804d
paulafanasyev <paulafanas@duck.com>
2026-08-27 13:35:22 +0300

    test: make Expo __DEV__ available in Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  define: {
+    __DEV__: true,
+  },
   test: {
     setupFiles: ["./vitest.setup.ts"],
+    server: {
+      deps: {
+        inline: ["expo"],
+      },
+    },
   },
 });
