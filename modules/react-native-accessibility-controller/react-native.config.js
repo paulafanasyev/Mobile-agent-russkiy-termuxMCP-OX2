275fea8fd9bb026845df76c52581e7fa65d162eb
paulafanasyev <paulafanas@duck.com>
2026-09-02 20:02:16 +0300

    test: configure legacy controller Android autolinking

---
+++ modules/react-native-accessibility-controller/react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dependency: {
+    platforms: {
+      android: {
+        packageImportPath: 'import com.paulafanasyev.ox2.accessibility.AccessibilityControllerLegacyPackage;',
+        packageInstance: 'new AccessibilityControllerLegacyPackage()',
+      },
+    },
+  },
+};
